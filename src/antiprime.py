83141decd2ae01d07fe34c9b35612bd88ce8116f
Victor Garcia <victor.garcia18@estudiant.upf.edu>
2024-10-22 15:26:05 +0200

    primer intento

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,17 +1,35 @@
+import sys
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-
+	x = int(sys.argv[1])
+	def divisors(x):
+		i = 1
+		divs = 0
+		while x >= i:
+			if x % i == 0:
+				divs += 1
+			i = i + 1
+		return divs
+	noantiprime = False
+	divsx = divisors(x)
+	for i in range(1,x):
+		if divisors(i) >=  divsx:
+			noantiprime = True
+			break
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	if noantiprime:
+		return("not anti-prime")
+	else:
+		return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
